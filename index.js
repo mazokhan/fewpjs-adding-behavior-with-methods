7cd6d84390f3b6c5d7c30d019e3f282a7733d8ee
mazen <mak9986@hotmail.com>
2021-11-07 22:53:18 +1100

    complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class cat {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+}
+
+speak() {
+    return `${this.name} says meow!`
+}
+
+class dog {
+    constructor(name,sex){
+        this.name = name
+        this.sex = sex
+    }
+}
+
+speak() {
+    return `${this.name}says woof!`
+}
+
+class bird{
+    constructor(name,sex) {
+        this.name = name
+        this.sex = sex
+    }
+}
+
+speak () {
+    (if this.sex === 'male'){
+        return `its me ${this.name}is the parrot`
+    }else {
+        return `${this.name} says sqwak!`
+    }
+}
